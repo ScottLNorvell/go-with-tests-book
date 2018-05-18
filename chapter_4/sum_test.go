7a5c0a2f9a1834cecc1f6ab714b07e12969fdb7f
Scott Norvell <scottlnorvell@gmail.com>
2018-05-18 09:07:19 -0400

    chapter 4 sum and example

--- chapter_4/sum_test.go
+++ chapter_4/sum_test.go
@@ -1,13 +1,38 @@
 package main
 
-import "testing"
+import (
+  "fmt"
+  "testing"
+)
 
-func TestSum(t *testing.T) {
-  numbers := [5]int{1,2,3,4,5}
-  got := Sum(numbers)
-  want := 15
-
+func assertSumsMatch(t *testing.T, got, want int, numbers []int) {
+  t.Helper()
   if got != want {
     t.Errorf("got '%d', want '%d', %v", got, want, numbers)
   }
+}
+
+func TestSum(t *testing.T) {
+  t.Run("collection of 5 numbers", func (t *testing.T) {
+    numbers := []int{1,2,3,4,5}
+    got := Sum(numbers)
+    want := 15
+
+    assertSumsMatch(t, got, want, numbers)
+  })
+
+  t.Run("collection of any size", func (t *testing.T) {
+    numbers := []int{1,2,3}
+    got := Sum(numbers)
+    want := 6
+
+    assertSumsMatch(t, got, want, numbers)
+  })
+}
+
+func ExampleSum() {
+  numbers := []int{5,10,15}
+  sum := Sum(numbers)
+  fmt.Println(sum)
+  // Output: 30
 }
