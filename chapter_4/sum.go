3372aa8ad5555fdc28c86c04bd93be0fb3df84b3
Scott Norvell <scottlnorvell@gmail.com>
2018-05-21 08:34:21 -0400

    chapter 4 DUN

--- chapter_4/sum.go
+++ chapter_4/sum.go
@@ -6,3 +6,22 @@ func Sum(numbers []int) (sum int) {
   }
   return
 }
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+  for _, nums := range numbersToSum {
+    sums = append(sums, Sum(nums))
+  }
+  return
+}
+
+func SumAllTails(numbersToSum ...[]int) (tailSums []int) {
+  for _, nums := range numbersToSum {
+    if len(nums) == 0 {
+      tailSums = append(tailSums, 0)
+    } else {
+      tail := nums[1:]
+      tailSums = append(tailSums, Sum(tail))
+    }
+  }
+  return
+}
